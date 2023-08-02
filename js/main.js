1cc5f9cf1bc097335434a59573573fe243f9de99
luigi-coder <luigicoder28@gmail.com>
2023-08-02 20:53:47 -0300

    encendiendo todo

--- js/main.js
+++ js/main.js
@@ -10,12 +10,14 @@ const bati_btn = d.querySelector('.bati_boton');
 
 bati_btn.addEventListener('click', () => {
     batman.classList.toggle('batman-activo');
-
+    d.body.style.background = '#5B5B5B'
     // currentTime = 0; Reiniciar el sonido al principio
     batman.classList.contains('batman-activo')
         ? (sound_batman.play(), sound_boton.play())
         : (
             sound_batman.pause(), sound_boton.pause(), 
             sound_batman.currentTime = 0, 
-            sound_boton.currentTime = 0);
+            sound_boton.currentTime = 0,
+            d.body.style.background = '#000'
+          );
 });
